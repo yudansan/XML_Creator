5a57a12b0f979dd7a73f9b6d203f5f29b136175b
yudansan <dansan.yu@gmail.com>
2013-06-09 01:36:59 -0700

    Add

---
+++ XML_Creator/XML_Creator/XML_Creator.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace XML_Creator
+{
+    partial class XML_Creator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem_Build = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem_Info = new System.Windows.Forms.ToolStripMenuItem();
+            this.Floder_Select_Dlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TreeV = new System.Windows.Forms.TreeView();
+            this.ToolStripMenuItem_About = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menu.SuspendLayout();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_File,
+            this.ToolStripMenuItem_Info});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(304, 25);
+            this.Menu.TabIndex = 0;
+            this.Menu.Text = "Menu";
+            // 
+            // ToolStripMenuItem_File
+            // 
+            this.ToolStripMenuItem_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem_Open,
+            this.MenuItem_Build,
+            this.toolStripSeparator1,
+            this.MenuItem_Exit});
+            this.ToolStripMenuItem_File.Name = "ToolStripMenuItem_File";
+            this.ToolStripMenuItem_File.ShortcutKeyDisplayString = "F";
+            this.ToolStripMenuItem_File.Size = new System.Drawing.Size(39, 21);
+            this.ToolStripMenuItem_File.Text = "&File";
+            // 
+            // MenuItem_Open
+            // 
+            this.MenuItem_Open.Name = "MenuItem_Open";
+            this.MenuItem_Open.ShortcutKeyDisplayString = "";
+            this.MenuItem_Open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.MenuItem_Open.Size = new System.Drawing.Size(155, 22);
+            this.MenuItem_Open.Text = "&Open";
+            this.MenuItem_Open.Click += new System.EventHandler(this.MenuItem_Open_Click);
+            // 
+            // MenuItem_Build
+            // 
+            this.MenuItem_Build.Name = "MenuItem_Build";
+            this.MenuItem_Build.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.MenuItem_Build.Size = new System.Drawing.Size(155, 22);
+            this.MenuItem_Build.Text = "&Build";
+            this.MenuItem_Build.Click += new System.EventHandler(this.MenuItem_Build_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // MenuItem_Exit
+            // 
+            this.MenuItem_Exit.Name = "MenuItem_Exit";
+            this.MenuItem_Exit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.MenuItem_Exit.Size = new System.Drawing.Size(155, 22);
+            this.MenuItem_Exit.Text = "E&xit";
+            this.MenuItem_Exit.Click += new System.EventHandler(this.MenuItem_Exit_Click);
+            // 
+            // ToolStripMenuItem_Info
+            // 
+            this.ToolStripMenuItem_Info.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_About});
+            this.ToolStripMenuItem_Info.Name = "ToolStripMenuItem_Info";
+            this.ToolStripMenuItem_Info.Size = new System.Drawing.Size(43, 21);
+            this.ToolStripMenuItem_Info.Text = "&Info";
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel1,
+            this.StatusLabel2});
+            this.StatusStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.StatusStrip.Location = new System.Drawing.Point(0, 294);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(304, 22);
+            this.StatusStrip.TabIndex = 1;
+            this.StatusStrip.Text = "StatusStrip";
+            // 
+            // StatusLabel1
+            // 
+            this.StatusLabel1.Name = "StatusLabel1";
+            this.StatusLabel1.Size = new System.Drawing.Size(66, 17);
+            this.StatusLabel1.Text = "Welcome!";
+            // 
+            // StatusLabel2
+            // 
+            this.StatusLabel2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.StatusLabel2.Name = "StatusLabel2";
+            this.StatusLabel2.Size = new System.Drawing.Size(28, 17);
+            this.StatusLabel2.Text = "     ";
+            // 
+            // TreeV
+            // 
+            this.TreeV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TreeV.Location = new System.Drawing.Point(0, 25);
+            this.TreeV.Name = "TreeV";
+            this.TreeV.Size = new System.Drawing.Size(304, 269);
+            this.TreeV.TabIndex = 2;
+            this.TreeV.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeV_BeforeExpand);
+            this.TreeV.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeV_AfterSelect);
+            // 
+            // ToolStripMenuItem_About
+            // 
+            this.ToolStripMenuItem_About.Name = "ToolStripMenuItem_About";
+            this.ToolStripMenuItem_About.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItem_About.Text = "&About";
+            this.ToolStripMenuItem_About.Click += new System.EventHandler(this.ToolStripMenuItem_About_Click);
+            // 
+            // XML_Creator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 316);
+            this.Controls.Add(this.TreeV);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.Menu);
+            this.MainMenuStrip = this.Menu;
+            this.Name = "XML_Creator";
+            this.Text = "XML_Creator";
+            this.Load += new System.EventHandler(this.XML_Creator_Load);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_File;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_Open;
+        private System.Windows.Forms.FolderBrowserDialog Floder_Select_Dlg;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_Build;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_Exit;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel2;
+        private System.Windows.Forms.TreeView TreeV;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Info;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_About;
+    }
+}
+
---
+++ XML_Creator/XML_Creator/XML_Creator.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XML_Creator
+{
+    public partial class XML_Creator : Form
+    {
+        private static DirectoryInfo Select_Dir;
+        private static DriveInfo Select_Dri;
+        private static FileInfo Select_File;
+        private string Select_Floder;
+        private IniFiles Config_INI ;
+
+        public XML_Creator()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Open_Click(object sender, EventArgs e)
+        {
+            Floder_Select_Dlg.ShowDialog();
+            Select_Floder = Floder_Select_Dlg.SelectedPath;
+            Console.WriteLine(Select_Floder);
+            this.StatusLabel1.Text = Select_Floder;
+            if (Select_Floder != null && Select_Floder != "")
+            {
+                TreeV.Nodes.Clear();
+                DirectoryInfo tmp = new DirectoryInfo(Select_Floder);
+                if (tmp.ToString() == tmp.Root.ToString())
+                {
+                    TreeNode root = new TreeNode(tmp.Root.Name);
+                    TreeV.Nodes.Add(root);
+                    Traverse(root, tmp.Root);
+                    this.StatusLabel2.Text = "List.";
+                }
+                else
+                {
+                    TreeNode root = new TreeNode(tmp.Name);
+                    TreeV.Nodes.Add(root);
+                    Traverse(root, tmp);
+                    this.StatusLabel2.Text = "List.";
+                }
+            }
+        }
+
+        private void MenuItem_Build_Click(object sender, EventArgs e)
+        {
+            if (Select_Floder != null && Select_Floder != "")
+            {
+                Select_Dir = new DirectoryInfo(Select_Floder);
+                if (Select_Dir.ToString() == Select_Dir.Root.ToString())
+                {
+                    Console.WriteLine("It is a Driver!");
+                    Select_Dri = new DriveInfo(Select_Dir.Root.Name);
+                    XElement dirXml = Select_Dri.LToXml();//显示所有文件
+                    string XML_Name = Select_Dri.VolumeLabel + Select_Dri.Name.Substring(0, 1) + "(" + (Select_Dri.TotalSize / 1024 / 1024 / 1024).ToString() + "G)";
+                    string XML_Path = Config_INI.ReadString("XML", "XML_Build_Path", "");
+                    if (XML_Path == null || XML_Path == "")
+                    {
+                        MessageBox.Show("ERROR 101 :\nDO NOT Find XML_Build_Path in XML!");
+                        return;
+                    }
+                    string XML_FullPath = XML_Path + XML_Name + ".xml";
+                    if (File.Exists(XML_FullPath))
+                    {
+                        //MessageBox.Show("Existed!So Rewrite!");
+                        this.StatusLabel1.Text = "Existed!So Rewrite.";
+                        File.Delete(XML_FullPath);
+                        dirXml.Save(XML_FullPath);
+                        this.StatusLabel2.Text = "Created.(Driver)";
+                    }
+                    else
+                    {
+                        dirXml.Save(XML_FullPath);
+                        this.StatusLabel2.Text = "Created.(Driver)";
+                    }
+                }
+                else
+                {
+                    XElement dirXml = Select_Dir.LToXml();//显示所有文件
+                    string XML_Name = Select_Dir.Name;
+                    string XML_Path = Config_INI.ReadString("XML", "XML_Build_Path", "");
+                    if (XML_Path == null || XML_Path == "")
+                    {
+                        MessageBox.Show("ERROR 101 :\nDO NOT Find XML_Build_Path in XML!");
+                        return;
+                    }
+                    string XML_FullPath = XML_Path + XML_Name + ".xml";
+                    if (File.Exists(XML_FullPath))
+                    {
+                        //MessageBox.Show("Existed!So Rewrite!");
+                        this.StatusLabel1.Text = "Existed!So Rewrite.";
+                        File.Delete(XML_FullPath);
+                        dirXml.Save(XML_FullPath);
+                        this.StatusLabel2.Text = "Created.(Floder)";
+                    }
+                    else
+                    {
+                        dirXml.Save(XML_FullPath);
+                        this.StatusLabel2.Text = "Created.(Floder)";
+                    }
+                }
+            }
+            else
+                this.StatusLabel1.Text = "Path is NULL!";
+        }
+
+        private void MenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TreeV_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void TreeV_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            //e.Node.Nodes.Cast<TreeNode>().ToList().ForEach(x =>
+            //{
+            //    try
+            //    {
+            //        if (x.Nodes.Count == 0)
+            //        {
+            //            TreeNode[] nodes = new TreeNode[] { };
+            //            if (x.Tag.GetType() == typeof(DriveInfo))
+            //            {
+            //                var item = x.Tag as DriveInfo;
+            //                //Console.WriteLine(item.Name);
+            //                nodes = Directory.GetDirectories(item.Name)
+            //                    .Select(y => new DirectoryInfo(y))
+            //                    .Select(y => new TreeNode(y.Name) { Tag = y })
+            //                    .ToArray();
+            //            }
+            //            if (x.Tag.GetType() == typeof(DirectoryInfo))
+            //            {
+            //                var item = x.Tag as DirectoryInfo;
+            //                //Console.WriteLine(item.FullName);
+            //                nodes = Directory.GetDirectories(item.FullName)
+            //                    .Select(y => new DirectoryInfo(y))
+            //                    .Select(y => new TreeNode(y.Name) { Tag = y })
+            //                    .ToArray();
+            //            }
+                        
+            //            x.Nodes.AddRange(nodes);
+            //        }
+            //    }
+            //    catch { }
+            //});
+        }
+
+        private void XML_Creator_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Config_INI = new IniFiles(System.Environment.CurrentDirectory + "/config.ini");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("ERROR 100 :\nGet INI failed!");
+                this.Close();
+            }
+        }
+
+        public void Traverse(TreeNode node, DirectoryInfo dir)
+        {
+            if (dir == null)
+            {
+                // 如果目录为空，则说明没有子目录，应该返回到上一层   
+                return;
+            }
+            else
+            {
+                // TreeNode treeNode = new TreeNode(dir.Name);
+                // node.Nodes.Add(treeNode); // 添加结点   
+                // 子目录不空，则优先遍历所有的子目录，再遍历每个子目录中的文件   
+                DirectoryInfo[] subDir = dir.GetDirectories();
+                foreach (DirectoryInfo sub in subDir)
+                {
+                    if (sub.Attributes.ToString().LastIndexOf("System") == -1)
+                    {
+                        TreeNode treeNode = new TreeNode(sub.Name);
+                        node.Nodes.Add(treeNode); // 添加结点   
+                        Traverse(treeNode, sub); // 先遍历当前目录的子目录 
+                    }
+                }
+                // 遍历当前目录的文件   
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo f in files)
+                {
+                    string fileName = f.Name;
+                    TreeNode fileNode = new TreeNode(fileName);
+                    node.Nodes.Add(fileNode);
+                }
+            }
+        }
+
+        private void ToolStripMenuItem_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Author :dansan\nVersion:1.0\nDate    :13-6-9");
+        }  
+    }
+}
